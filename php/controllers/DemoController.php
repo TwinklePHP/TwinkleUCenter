53b48a385e9f9622fdf4deb92c45a21db970e561
actors315 <actors315@gmail.com>
2017-11-28 22:00:01 +0800

    编辑用户信息

--- php/controllers/DemoController.php
+++ php/controllers/DemoController.php
@@ -8,26 +8,27 @@
 
 namespace app\controllers;
 
-use Yii;
 use app\base\rpc\Controller;
+use twinkle\client\Client;
+use Yii;
 
 class DemoController extends Controller
 {
-    
+
     public function actionIndex()
     {
         return $this->renderPartial('sign');
     }
-    
-    public function  actionSignIn()
+
+    public function actionSignIn()
     {
         $username = Yii::$app->request->post('username', '');
         $password = Yii::$app->request->post('password', '');
-        
+
         $client = new \Yar_Client(Yii::$app->request->hostInfo . '/rpc/user');
-        
+
         $result = $client->login($username, $password);
-        
+
         if (isset($result['status']) && $result['status'] === 0) {
             $info = empty($result['user_info']) ? [] : $result['user_info'];
             return $this->renderPartial('sign', [
@@ -35,8 +36,30 @@ class DemoController extends Controller
                 'step' => 'login_success',
             ]);
         }
-        
+
         return $this->renderPartial('sign', ['step' => 'login_fail']);
     }
-    
+
+    public function actionEdit()
+    {
+
+        $client = new Client('Yar', 'http://www.twinklephp.con/rpc/user');
+
+        if (Yii::$app->request->isPost) {
+            $editInfo = Yii::$app->request->post();
+            $result = $client->editUser($editInfo['user_id'], $editInfo);
+            return $this->renderPartial('edit', [
+                'msg' => $result['status'] ? '保存失败' : '修改成功',
+                'step' => 'save'
+            ]);
+        }
+
+        $id = Yii::$app->request->get('user_id');
+        $userInfo = $client->getUserById($id);
+        return $this->renderPartial('edit', [
+            'user_info' => $userInfo['user_info'],
+            'step' => 'read'
+        ]);
+    }
+
 }
---
+++ php/views/demo/edit.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php if ('read' == $step): ?>
+    <form action="/demo/edit" method="post">
+        <input type="hidden" value="<?= $user_info['user_id']; ?>" name="user_id">
+        <input type="text" value="<?= $user_info['first_name']; ?>" name="first_name">
+        <input type="text" value="<?= $user_info['last_name']; ?>" name="last_name">
+        <input type="submit" value="保存">
+    </form>
+    <div>
+        <p>当前用户信息</p>
+        <p>
+        <pre>
+            <?= var_export($user_info, true); ?>
+        </pre>
+        </p>
+    </div>
+
+<?php else: ?>
+    echo $msg;
+<? endif; ?>
+</body>
+</html>
